01836bf21f1ce1b18820810a01e93220470e91ff
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-02-22 23:52:02 +0900

    log 삭제

--- App.js
+++ App.js
@@ -20,8 +20,6 @@ export default function App() {
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
 
-  console.log(toDos);
-
   useEffect(() => {
     loadToDos();
   }, []);
